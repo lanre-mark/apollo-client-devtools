a155d8d2e44306316831a11038a9f5d11668f348
Jenn Creighton <jenncreighton@jenns-mbp.home>
2020-07-28 18:34:48 -0400

    convert to TS

--- src/extension/background/background.ts
+++ src/extension/background/background.ts
@@ -3,20 +3,23 @@ import { REQUEST_TAB_ID } from '../constants';
 
 // This sends the tab id to the inspected tab.
 chrome.runtime.onMessage.addListener(({ message }, sender, sendResponse) => {
+  console.log('for never leaving well enough alone');
   if (message === REQUEST_TAB_ID) {
+    console.log('but if would\'ve be fun, if you had been the one');
     sendResponse(sender?.tab?.id);
   }
 });
 
 const background = new Relay();
 
-chrome.runtime.onConnect.addListener((port) => {
+chrome.runtime.onConnect.addListener(port => {
+  console.log(port.name);
   background.addConnection(port.name, message => {
     port.postMessage(message);
   });
   
-  port.onMessage.addListener((message, sender) => {
-    background.broadcast(message, sender);
+  port.onMessage.addListener(message => {
+    background.broadcast(message);
   });
 
   port.onDisconnect.addListener(port => {
